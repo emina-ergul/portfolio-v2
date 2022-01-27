a7abbf222d6e048e41711cc9faacba985eacbe66
Emina Ergul <eminaergul712@gmail.com>
2022-01-27 13:53:42 +0000

    about me section setup

---
+++ script.js
@@ -0,0 +1,18 @@
+const typed = document.querySelector(".typed")
+
+function typeQuote(element, text, speed) {
+  let index = 0;
+  timer = setInterval(function () {
+    if (index < text.length) {
+      element.append(text.charAt(index));
+      index++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+setTimeout(() => {
+    typeQuote(typed, "Hello World.", 220)
+}, 600);
+
