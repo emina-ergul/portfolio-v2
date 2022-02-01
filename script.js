717a0fde43aa49a883ba4fb8c742b069283bf9bd
Emina Ergul <eminaergul712@gmail.com>
2022-02-01 10:27:45 +0000

    skills sec/removed css animation

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// const typed = document.querySelector(".typed")
+const typed = document.querySelector(".typed")
 
-// function typeQuote(element, text, speed) {
-//   let index = 0;
-//   timer = setInterval(function () {
-//     if (index < text.length) {
-//       element.append(text.charAt(index));
-//       index++;
-//     } else {
-//       clearInterval(timer);
-//     }
-//   }, speed);
-// }
+function typeQuote(element, text, speed) {
+  let index = 0;
+  timer = setInterval(function () {
+    if (index < text.length) {
+      element.append(text.charAt(index));
+      index++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
 
-// setTimeout(() => {
-//     typeQuote(typed, "Hello World.", 220)
-// }, 900);
+setTimeout(() => {
+    typeQuote(typed, "Hello World.", 220)
+}, 900);
 
